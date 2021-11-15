04c63c5035cce4df967d2e010d2f066dc18bbe53
Taina Leal <tainacleal@gmail.com>
2021-11-15 10:40:56 -0500

    supports graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -26,7 +31,30 @@ func main() {
 	server := &http.Server{Addr: serverAddress, Handler: r}
 
 	fmt.Printf("Serving at http://%s/", serverAddress)
-	server.ListenAndServe()
+
+	// start service in a go routine to support graceful shutdown
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("error shutting down bot service")
+		}
+	}()
+
+	// wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-quit
+	fmt.Printf("caught signal %s, shutting down...", sig)
+
+	// The context is used to inform the server it has X seconds to finish the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Println("error shutting down server cleanly", err)
+	} else {
+		fmt.Println("gracefully shut down bot service")
+	}
 }
 
 // ----//----
